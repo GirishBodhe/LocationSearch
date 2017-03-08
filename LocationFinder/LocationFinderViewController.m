d26ee030a6c09e54f5b4e6706a5c7d35b9dcf6b2
GirishBodhe <abhijitsapkal@Abhijits-MacBook-Air.local>
2017-03-09 01:27:46 +0530

    log done
    commit 1

---
+++ LocationFinder/LocationFinderViewController.m
@@ -0,0 +1,111 @@
+//
+//  LocationFinderViewController.m
+//  LocationFinder
+//
+//  Created by TheAppGuruz-iOS-103 on 15/05/14.
+//  Copyright (c) 2014 TheAppGuruz. All rights reserved.
+//
+
+#import "LocationFinderViewController.h"
+#import <FTGooglePlacesAPI/FTGooglePlacesAPI.h>
+@interface LocationFinderViewController ()
+{
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
+}
+@end
+
+@implementation LocationFinderViewController
+@synthesize locationManager;
+@synthesize txtState, txtLatitude, txtLongitude, txtCountry;
+
+#pragma mark - View LifeCycle
+
+- (void)viewDidLoad
+{
+    //AIzaSyBDlaMiDp3WT4Y_l9Ats-y_wbHcSJZKJTU
+    
+    [super viewDidLoad];
+    geocoder = [[CLGeocoder alloc] init];
+    if (locationManager == nil)
+    {
+        locationManager = [[CLLocationManager alloc] init];
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        locationManager.delegate = self;
+        [locationManager requestAlwaysAuthorization];
+    }
+    [locationManager startUpdatingLocation];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    // Turn off the location manager to save power.
+    [self.locationManager stopUpdatingLocation];
+}
+
+#pragma mark - CLLocationManager delegate methods
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+
+    CLLocation *newLocation = [locations lastObject];
+    
+    [geocoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        
+        if (error == nil && [placemarks count] > 0) {
+            placemark = [placemarks lastObject];
+           
+            txtState.text = placemark.administrativeArea;
+            txtCountry.text = placemark.country;
+            //  Create location around which to search (hardcoded location of Big Ben here)
+//            CLLocationCoordinate2D locationCoordinate = CLLocationCoordinate2DMake(19.159960,72.997286);
+ CLLocationCoordinate2D locationCoordinate = CLLocationCoordinate2DMake(newLocation.coordinate.latitude,newLocation.coordinate.longitude);
+            //  Create request searching nearest galleries and museums
+            FTGooglePlacesAPINearbySearchRequest *request = [[FTGooglePlacesAPINearbySearchRequest alloc] initWithLocationCoordinate:locationCoordinate];
+            request.rankBy = FTGooglePlacesAPIRequestParamRankByDistance;
+            request.types = @[@"art_gallery", @"museum",@"hotel"];
+            //  Execute Google Places API request using FTGooglePlacesAPIService
+            [FTGooglePlacesAPIService executeSearchRequest:request
+                                     withCompletionHandler:^(FTGooglePlacesAPISearchResponse *response, NSError *error) {
+                                         
+                                         //  If error is not nil, request failed and you should handle the error
+                                         if (error)
+                                         {
+                                             // Handle error here
+                                             NSLog(@"Request failed. Error: %@", error);
+                                             
+                                             //  There may be a lot of causes for an error (for example networking error).
+                                             //  If the network communication with Google Places API was successfull,
+                                             //  but the API returned some non-ok status code, NSError will have
+                                             //  FTGooglePlacesAPIErrorDomain domain and status code from
+                                             //  FTGooglePlacesAPIResponseStatus enum
+                                             //  You can inspect error's domain and status code for more detailed info
+                                         }
+                                         
+                                         //  Everything went fine, we have response object we can process
+                                         NSLog(@"Request succeeded. Response: %lu", (unsigned long)[response.results count]);
+                                         txtLatitude.text = [NSString stringWithFormat:@"%f  (%lu)",newLocation.coordinate.latitude,(unsigned long)[response.results count]];
+                                         txtLongitude.text = [NSString stringWithFormat:@"%f",newLocation.coordinate.longitude];
+                                         
+                                         
+
+                                     }];
+            
+        } else {
+            NSLog(@"%@", error.debugDescription);
+        }
+    } ];
+    
+    // Turn off the location manager to save power.
+    [manager stopUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+        didFailWithError:(NSError *)error
+{
+    NSLog(@"Cannot find the location.");
+}
+
+
+@end
---
+++ Pods/Target Support Files/FTGooglePlacesAPI/FTGooglePlacesAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FTGooglePlacesAPI : NSObject
+@end
+@implementation PodsDummy_FTGooglePlacesAPI
+@end
